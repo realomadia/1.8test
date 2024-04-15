1cefeb055cb4e2b05d0dda1cdc188d784a02ace9
lomadia <hhun6840@naver.com>
2024-04-15 14:01:36 +0900

    jdk 1.8 test

---
+++ src/main/java/com/bitsign/BitSignApplication.java
@@ -0,0 +1,13 @@
+package com.bitsign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitSignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitSignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bitsign/BitSignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitsign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitSignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
